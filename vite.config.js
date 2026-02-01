a3b3941e8cf86f5532fe8b71c8e288b75e749010
reagan <221689087+reastack25@users.noreplygithub.com>
2026-02-01 14:51:10 +0300

    deployment

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     
+    
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
-    }),
-    
+    }),  
   ],
+  base: "/todoapp"
 })
